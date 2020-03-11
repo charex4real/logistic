4ff68acffb5fc5b50d221f0b00581d32c6050deb
charex4real <charex4real@gmail.com>
2020-03-11 09:54:09 +0100

    03-11-2020--9:54

--- core/resources/views/frontend/layouts/header.blade.php
+++ core/resources/views/frontend/layouts/header.blade.php
@@ -219,9 +219,7 @@
                   <div class="header-row">
                     <nav class="header-nav-top">
                       <ul class="nav nav-pills">
-                        <li class="nav-item nav-item-borders py-2 d-none d-sm-inline-flex">
-                          <span class="pl-0"><i class="far fa-dot-circle text-4 text-color-primary" style="top: 1px;"></i> {{$gs->contact_address}}</span>
-                        </li>
+                       
                         <li class="nav-item nav-item-borders py-2">
                           <a href="tel:123-456-7890"><i class="fab fa-whatsapp text-4 text-color-primary" style="top: 0;"></i> {{$gs->contact_phone}}</a>
                         </li>
